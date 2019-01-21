d723093858ca2d267b2d35876cc4e8114ec8ca19
amrut7764 <chougale.amrut@gmail.com>
2019-01-21 16:25:33 -0500

    Fix Unittest to check exception NoCredentialsError

--- test_flask.py
+++ test_flask.py
@@ -9,13 +9,11 @@ class TestAWSUnittest(unittest.TestCase):
 
     def test_ec2InstaneList(self):
         ec2 = awsEC2()
-        # self.assertRaises(AttributeError, ec2.allInstances)
-        self.assertNotEqual(ec2.allInstances(), False)
-
+        self.assertRaises(NoCredentialsError, ec2.allInstances)
+        
     def test_rdsInstances(self):
         rds = awsRDS(region="us-east-1")
-        ret = rds.RDSInstances()
-        self.assertIsNot(ret, False)
+        self.assertRaises(NoCredentialsError, rds.RDSInstances)
 
 
 if __name__ == "__main__":
