1e27390bff8701bb471fdadc0ee75b11c54afe15
Amrut Chougale <chougale.amrut@gmail.com>
2019-01-21 22:39:12 -0500

    Add Test case for S3 create bucket

--- test_flask.py
+++ test_flask.py
@@ -1,19 +1,25 @@
+from botocore.exceptions import NoCredentialsError
 from lib import *
 import unittest
 import os
 import sys
 sys.path.append(os.path.abspath("../"))
-from botocore.exceptions import NoCredentialsError
+
 
 class TestAWSUnittest(unittest.TestCase):
 
     def test_ec2InstaneList(self):
         ec2 = awsEC2()
         self.assertRaises(NoCredentialsError, ec2.allInstances)
-        
+
     def test_rdsInstances(self):
         rds = awsRDS(region="us-east-1")
         self.assertRaises(NoCredentialsError, rds.RDSInstances)
+
+    def test_s3CreateBucket(self):
+        s3 = awsS3(objType="client", ver=3)
+        self.assertRaises(NoCredentialsError, s3.createBucket,
+                          Bucket="thisismytestbucket-1231231234234234")
 
 
 if __name__ == "__main__":
